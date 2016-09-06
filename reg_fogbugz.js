bdfe1b9c41f81338a161d4d6e1f71e0926a5fa9f
Wally Chang <wally.chang@protolabs.co.jp>
2016-09-06 11:02:46 +0900

    remove unnecessary regex files

--- reg_fogbugz.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-var string = "BugzID: 123456";
-var regex = /^BugzID: ([0-9]+)$/
-var result = string.match(regex);
-console.log('Hello, world!'+' result:'+result+'  '+result[1]+'  '+result[2]);
